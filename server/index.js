444bf046b6c25c17ed9ac98dfde52f4f5f0ab6d9
shubham waghadhare <waghadhare.shubham@gmail.com>
2026-05-10 15:10:22 +0530

    live server

--- server/index.js
+++ server/index.js
@@ -7,10 +7,12 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] },
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
-
 const PORT = 5000;
 
 const SUITS = ["s", "h", "d", "c"];
